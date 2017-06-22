2854c685ac15581cff618af67e64e97da87f4624
l.sapurina <yoyxoxo@gmail.com>
2017-06-22 20:03:38 +0300

    instr

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from qa.views import test
 
 urlpatterns = [
     url(r'^$', include('qa.urls')),
