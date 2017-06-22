ebfbe8f509f1a541ef233c1979f9e737eb5b893d
l.sapurina <yoyxoxo@gmail.com>
2017-06-22 15:55:35 +0300

    delete sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
+# Меняем ссылку на конфиг nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# Перезапускаем nginx
 sudo /etc/init.d/nginx restart
+
+# Меняем ссылку на конфиг gunicorn
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 sudo /etc/init.d/gunicorn restart
