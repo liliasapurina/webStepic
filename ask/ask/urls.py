a4f92a12a67ae08ff405d43de62f8a5fdf39b469
l.sapurina <yoyxoxo@gmail.com>
2017-06-22 18:13:29 +0300

    routing

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,5 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^question/\d+/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls'))
 ]
