121c5cecdd858613bf26a752ba900f5d7a495aaf
l.sapurina <yoyxoxo@gmail.com>
2017-06-22 20:23:36 +0300

    instr

--- init.sh
+++ init.sh
@@ -9,6 +9,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo /etc/init.d/nginx restart
 
 # Gunicorn (ver. 17.5)
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+#sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+#sudo /etc/init.d/gunicorn restart
