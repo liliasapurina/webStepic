e391c2a70b2c945f64bba54a7e7b50c8bf32c4c3
l.sapurina <yoyxoxo@gmail.com>
2017-06-22 21:08:34 +0300

    instr

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,10 +19,10 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', include('qa.urls')),
-    url(r'^login/', include('qa.urls')),
-    url(r'^signup/', include('qa.urls')),
+    url(r'^login/$', include('qa.urls')),
+    url(r'^signup/$', include('qa.urls')),
     url(r'^question/(?P<num>\d+)/$', include('qa.urls')),
-    url(r'^ask/', include('qa.urls')),
-    url(r'^popular/', include('qa.urls')),
-    url(r'^new/', include('qa.urls'))
+    url(r'^ask/$', include('qa.urls')),
+    url(r'^popular/$', include('qa.urls')),
+    url(r'^new/$', include('qa.urls'))
 ]
