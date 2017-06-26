c780751d85764a15fe2530ba055de74416eead75
l.sapurina <yoyxoxo@gmail.com>
2017-06-26 17:24:48 +0300

    mysql

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,6 @@
 
 sudo /etc/init.d/mysql restart
 mysql -uroot -e "create database myproject;"
-mysql -uroot -e "CREATE USER 'enth'@'localhost' IDENTIFIED BY 'password';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'lsapurina'@'localhost';"
+mysql -uroot -e "CREATE USER 'lsapurina@localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'lsapurina@localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
