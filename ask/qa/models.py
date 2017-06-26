782d766bc06ee9521a28a794a1d376b299d5ff2c
l.sapurina <yoyxoxo@gmail.com>
2017-06-26 16:50:35 +0300

    mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new():
+        pass
+    def popular():
+        pass
+
+class Question(models.Model):
+    objects = QuestionManager()
+
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name='author')
+    likes = models.ManyToManyField(User, related_name='likes')
+
+    class Meta:
+        db_table = 'Question'
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        db_table = 'Answer'
--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+# Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-#gunicorn -c /etc/gunicorn.d/hello.py hello:app
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
+
+# Mysql
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'qwerty';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
