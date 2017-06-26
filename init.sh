a5713d27f4c5be3c78adca8d59f0aa7e32996d82
l.sapurina <yoyxoxo@gmail.com>
2017-06-26 16:54:17 +0300

    mysql

--- init.sh
+++ init.sh
@@ -5,10 +5,3 @@ sudo /etc/init.d/nginx restart
 # Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
-
-# Mysql
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djbase;"
-mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'qwerty';"
-mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
---
+++ mysql.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'qwerty';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
