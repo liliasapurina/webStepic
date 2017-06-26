0d6d0a4633b78f29d7ee7102b5c0737f334dccff
l.sapurina <yoyxoxo@gmail.com>
2017-06-26 12:42:40 +0300

    instr

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Nginx
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/backend.py  /etc/gunicorn.d/backend.py
 sudo /etc/init.d/gunicorn restart
