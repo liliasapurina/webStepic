46707d69bf9cd37cf08e649b8f53c6fbe89b6ab6
l.sapurina <yoyxoxo@gmail.com>
2017-06-26 17:26:01 +0300

    mysql

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'web',
-        'USER': 'lsapurina',
+        'USER': 'lilia',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
